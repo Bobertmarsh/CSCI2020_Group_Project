e17a5117af038e0c9aaafc9a6a05c6730257fafd
Bobertmarsh <Robert@Robert.localdomain>
2019-03-25 21:08:16 -0400

    Added the ability to export text log to .txt from the server window. Added ability to press the enter key instead of clicking the send button. Documented code.

--- Client1.java
+++ Client1.java
@@ -1,33 +1,26 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client1 extends Application {
 
-    private Canvas canvas;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Group root = new Group();
-        primaryStage.setTitle("Simple BBS Client v1.0");
+        primaryStage.setTitle("Group Project - Chat Client");
         GridPane gridPane = new GridPane();
         gridPane.setVgap(20);
         gridPane.setHgap(50);
@@ -35,31 +28,36 @@ public class Client1 extends Application {
 
 
         Label userNameLbl = new Label("Username: ");
-        TextField userNameFld = new TextField();
+        TextField txtUserName = new TextField();
         Label messageLbl = new Label("Message: ");
-        TextField messageFld = new TextField();
-        Button sendBtn = new Button("Send");
-        sendBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                String myMessage = userNameFld.getText() + ": " + messageFld.getText();
-                //System.out.println("Text being sent is " + messageFld.getText());
-                System.out.println("Text being sent is " + myMessage);
-                //sendMessage(messageFld.getText());
-                sendMessage(myMessage);
-                messageFld.setText("");
-            }
+        TextField txtMessage = new TextField();
+        Button btnSend = new Button("Send");
+        // If enter is pressed while txtMessage is selected it will attempt to send the message
+        txtMessage.setOnKeyPressed(event -> {
+        	if (event.getCode() == KeyCode.ENTER) {
+		        String message = txtUserName.getText() + ": " + txtMessage.getText();
+		        System.out.println("Text being sent is " + message);
+		        sendMessage(message);
+		        txtMessage.setText("");
+	        }
         });
-        Button exitBtn = new Button("Exit");
-        exitBtn.setOnAction(evt -> System.exit(0));
+        // If the send button is pressed it will attempt to send the message in txtMEssage
+        btnSend.setOnAction(event -> {
+            String message = txtUserName.getText() + ": " + txtMessage.getText();
+            System.out.println("Text being sent is " + message);
+            sendMessage(message);
+            txtMessage.setText("");
+        });
+        Button btnExit = new Button("Exit");
+        btnExit.setOnAction(evt -> System.exit(0));
         
 
         gridPane.add(userNameLbl, 0, 0);
-        gridPane.add(userNameFld, 1, 0);
+        gridPane.add(txtUserName, 1, 0);
         gridPane.add(messageLbl, 0, 1);
-        gridPane.add(messageFld, 1, 1);
-        gridPane.add(sendBtn, 0, 2);
-        gridPane.add(exitBtn, 0, 3);
+        gridPane.add(txtMessage, 1, 1);
+        gridPane.add(btnSend, 0, 2);
+        gridPane.add(btnExit, 0, 3);
 
 
         root.getChildren().addAll(gridPane);
@@ -75,11 +73,11 @@ public class Client1 extends Application {
             out.println(message);
             out.flush();
             socket.close();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 
 
     public static void main(String[] args) {
--- Server1.java
+++ Server1.java
@@ -1,23 +1,25 @@
 
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Server1 extends Application {
 
-    TextArea textArea;
+    TextArea txtLog;
 
 
     public static void main(String[] args) {
@@ -26,33 +28,68 @@ public class Server1 extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+    	// Sets ip scene
         Group root = new Group();
-        primaryStage.setTitle("Simple BBS Server v1.0");
+        primaryStage.setTitle("Group Project - Chat Server");
         GridPane gridPane = new GridPane();
-        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setPadding(new Insets(0, 10, 10, 10));
         gridPane.setVgap(20);
 
-        textArea = new TextArea();
-        Button exitBtn = new Button("Exit");
-        exitBtn.setOnAction(evt -> System.exit(0));
-        gridPane.add(textArea, 0, 0);
-        gridPane.add(exitBtn, 0, 1);
+        // Creates a menu bar with the option to export -> export to .txt
+        MenuBar bar = new MenuBar();
+        Menu Export = new Menu("Export");
+        MenuItem toTxt = new MenuItem("To .txt");
+        bar.getMenus().add(Export);
+        Export.getItems().add(toTxt);
+        // Creates a text area for the chat log
+        txtLog = new TextArea();
+        // Button to exit the program properly
+        Button btnExit = new Button("Exit");
+        btnExit.setOnAction(evt -> System.exit(0));
+        //Adds above items to the gridPane
+        gridPane.add(bar, 0,0);
+        gridPane.add(txtLog, 0, 1);
+        gridPane.add(btnExit, 0, 2);
 
+        // Exports chat log to a .txt file
+        toTxt.setOnAction(new EventHandler<ActionEvent>() {
+	        @Override
+	        public void handle(ActionEvent event) {
+		        // Creates a file name for exporting based on date and time
+		        DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd_HH.mm.ss");
+		        Date date = new Date();
+		        String filename = "Log_" + dateFormat.format(date) + ".txt";
+		        try
+		        {
+			        //Creates a print writer, writes to the file, and closes the writer
+			        PrintWriter outputStream = new PrintWriter(filename);
+			        outputStream.print(txtLog.getText());
+			        outputStream.flush();
+			        outputStream.close();
+			        System.out.println("Export Complete");
+		        }
+		        catch (FileNotFoundException e)
+		        {
+			        // Error handling
+			        e.printStackTrace();
+		        }
+	        }
+        });
+		// Adds everything from the grid to the scene
         root.getChildren().add(gridPane);
         Scene scene = new Scene(root, 500, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
-
+		// Starts a thread to look for connections
         Thread t = new Thread(new handleConnections());
         t.start();
 
     }
+    //
     public class handleConnections implements Runnable {
-        String message;
 
         private ServerSocket serverSocket;
-        Thread[] threads = null;
-
+		// Looks for connection attempts
         public handleConnections() {
             try {
                 this.serverSocket = new ServerSocket(60000);
@@ -61,6 +98,7 @@ public class Server1 extends Application {
                 e.printStackTrace();
             }
         }
+        // If a connection happens get the text the client sent and pass it to the handler
         @Override
         public void run() {
             System.out.println("Thread is running");
@@ -73,13 +111,6 @@ public class Server1 extends Application {
                     Thread t = new Thread(new getText(clientSocket));
                     t.start();
 
-                    //BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    /*while ((message = in.readLine()) != null) {
-                        textArea.appendText(message +"\n");
-                        System.out.println("The message is: " + message);
-                    }*/
-
-
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -87,6 +118,7 @@ public class Server1 extends Application {
             }
         }
     }
+    // Handles the text received from a client
     public class getText implements Runnable {
         private Socket clientSocket;
         private String message;
@@ -96,16 +128,19 @@ public class Server1 extends Application {
         }
         @Override
         public void run() {
+        	// Reads text sent from a client and appends it to the end of the text log
             try {
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 while ((message = in.readLine()) != null) {
-                    textArea.appendText(message + "\n");
+                    txtLog.appendText(message + "\n");
                     System.out.println("Message being read is " + message);
                 }
-
+			// Error handling
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+
 
         }
     }
